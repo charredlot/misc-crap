cd11b19213c94c3e6c5c786b978cb59b8dcd30fe
charredlot <charredlot@gmail.com>
2017-09-25 23:59:59 -0700

    cryptopals: in ecb_decrypt.rs pass cipher not key

--- rust/cryptopals/src/aes/ecb_decrypt.rs
+++ rust/cryptopals/src/aes/ecb_decrypt.rs
@@ -12,29 +12,29 @@ const ORACLE_SUFFIX: &'static str = "Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9
 
 const ORACLE_SUFFIX_STR: &'static str = "Rollin' in my 5.0\nWith my rag-top down so my hair can blow\nThe girlies on standby waving just to say hi\nDid you stop? No, I just drove by\n";
 
-fn encrypt_aes_ecb_suffix_oracle(key: &[u8], plaintext: &[u8]) -> Vec<u8> {
-    let cipher = AESCipher::new(&key);
+fn encrypt_aes_ecb_suffix_oracle(cipher: &AESCipher,
+                                 plaintext: &[u8]) -> Vec<u8> {
     let mut suffixed = plaintext.to_vec();
     suffixed.extend_from_slice(&base64_decode(ORACLE_SUFFIX));
     cipher.ecb_encrypt(&pkcs7_pad(&suffixed, 16))
 }
 
-fn confirm_aes_ecb(key: &[u8]) {
+fn confirm_aes_ecb(cipher: &AESCipher) {
     let two_blocks = [0u8; 32];
-    let ciphertext = encrypt_aes_ecb_suffix_oracle(key, &two_blocks);
+    let ciphertext = encrypt_aes_ecb_suffix_oracle(cipher, &two_blocks);
     if ciphertext[0..16] != ciphertext[16..32] {
         panic!("expected aes ecb, got ciphertext {:?}", &ciphertext);
     }
 }
 
-fn get_aes_ecb_suffix_len(key: &[u8]) -> usize {
+fn get_aes_ecb_suffix_len(cipher: &AESCipher) -> usize {
     let mut v: Vec<u8> = Vec::new();
 
     let mut result: usize = 0;
-    let start_len = encrypt_aes_ecb_suffix_oracle(key, &v).len();
+    let start_len = encrypt_aes_ecb_suffix_oracle(cipher, &v).len();
     for i in 1..17 {
         v.push(0u8);
-        let n = encrypt_aes_ecb_suffix_oracle(key, &v).len();
+        let n = encrypt_aes_ecb_suffix_oracle(cipher, &v).len();
         let diff = n - start_len;
         if diff > 0 {
             // if suffix_len % 16 == 0, adding 16 will give a 16 byte diff
@@ -48,8 +48,8 @@ fn get_aes_ecb_suffix_len(key: &[u8]) -> usize {
     result
 }
 
-fn decrypt_aes_ecb_suffix(key: &[u8]) -> Vec<u8> {
-    let n = get_aes_ecb_suffix_len(key);
+fn decrypt_aes_ecb_suffix(cipher: &AESCipher) -> Vec<u8> {
+    let n = get_aes_ecb_suffix_len(cipher);
     let mut block = [0u8; 16];
     let mut block_map: HashMap<Vec<u8>, u8>  = HashMap::new();
     let mut result: Vec<u8> = Vec::new();
@@ -61,12 +61,12 @@ fn decrypt_aes_ecb_suffix(key: &[u8]) -> Vec<u8> {
             let byte = j as u8;
             block[15] = byte;
 
-            let ciphertext = encrypt_aes_ecb_suffix_oracle(key, &block);
+            let ciphertext = encrypt_aes_ecb_suffix_oracle(cipher, &block);
             // for some reason "j as u8" returns an Option
             block_map.insert((&ciphertext[0..16]).to_vec(), byte);
         }
         let end = 15 - (i % 16);
-        let ciphertext = encrypt_aes_ecb_suffix_oracle(key, &block[0..end]);
+        let ciphertext = encrypt_aes_ecb_suffix_oracle(cipher, &block[0..end]);
 
         let cblock = block_index * 16;
         match block_map.get(&ciphertext[cblock..cblock + 16]) {
@@ -97,8 +97,9 @@ pub fn decrypt_aes_ecb_simple_test() {
     }
     println!("AES ECB simple decrypt test with key {:?}", &key);
 
-    confirm_aes_ecb(&key);
-    let decrypted_bytes = decrypt_aes_ecb_suffix(&key);
+    let cipher = AESCipher::new(&key);
+    confirm_aes_ecb(&cipher);
+    let decrypted_bytes = decrypt_aes_ecb_suffix(&cipher);
     let decrypted = str::from_utf8(&decrypted_bytes).unwrap();
     if decrypted != ORACLE_SUFFIX_STR {
         panic!("decrypt aes ecb suffix failed, got {:?}", &decrypted_bytes);
