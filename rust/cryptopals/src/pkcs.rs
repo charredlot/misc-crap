fb292ad87726a078b196d0865263e9371d9a3c1d
charredlot <charredlot@gmail.com>
2017-10-08 23:10:09 -0700

    cryptopals: add pkcs7 unpad that can return err

--- rust/cryptopals/src/pkcs.rs
+++ rust/cryptopals/src/pkcs.rs
@@ -1,4 +1,5 @@
 use hex::{hex_to_bytes,bytes_to_hex};
+use std::string::String;
 
 pub fn pkcs7_pad(buf: &[u8], block_size: usize) -> Vec<u8> {
     let pad_len = (block_size - (buf.len() % block_size)) as u8;
@@ -13,19 +14,30 @@ pub fn pkcs7_pad(buf: &[u8], block_size: usize) -> Vec<u8> {
 }
 
 pub fn pkcs7_unpad<'a>(buf: &'a [u8]) -> &'a [u8] {
+    match pkcs7_maybe_unpad(buf) {
+        Ok(result) => result,
+        Err(e) => panic!(e),
+    }
+}
+
+pub fn pkcs7_maybe_unpad<'a>(buf: &'a [u8]) -> Result<&'a [u8], String> {
     if buf.len() == 0 {
-        panic!("pkcs7_unpad len 0 buf");
+        return Err(String::from("pkcs7_unpad len 0 buf"));
     }
     let pad = *buf.last().unwrap() as usize;
     if pad > buf.len() {
-        panic!("pkcs7_unpad padding too long {:?}", buf);
+        return Err(format!("pkcs7_unpad padding too long {:?}", buf));
+    }
+    if pad == 0 {
+        return Err(format!("pkcs7_unpad 0 is not valid padding {:?}", buf));
     }
     for (i, &b) in (&buf[buf.len() - pad..]).iter().enumerate() {
         if b != pad as u8 {
-            panic!("bad byte at {}: {:?}", buf.len() - pad + i, buf);
+            return Err(format!("bad byte at {}: {:?}",
+                               buf.len() - pad + i, buf));
         }
     }
-    &buf[0..(buf.len() - pad)]
+    Ok(&buf[0..(buf.len() - pad)])
 }
 
 pub fn pkcs7_test() {
