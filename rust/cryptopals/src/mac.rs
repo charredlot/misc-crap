3175587089928e17d13adf9c92103531cc3724a6
charredlot <charredlot@gmail.com>
2017-10-17 21:38:38 -0700

    cryptopals: add sha1_pad function

--- rust/cryptopals/src/mac.rs
+++ rust/cryptopals/src/mac.rs
@@ -9,3 +9,38 @@ pub fn sha1_cat_mac(key: &[u8], message: &[u8]) -> Vec<u8> {
     hash.update(&v);
     hash.digest().bytes().to_vec()
 }
+
+pub fn sha1_pad(msg: &[u8]) -> Vec<u8> {
+    let mut padded = msg.to_vec();
+
+    let mut pad = [0u8; 64];
+    let len = msg.len();
+    let rem = len % 64;
+
+    // add a 1 bit then 0 bits until we get len % 64 == 56
+    pad[0] = 0x80;
+    if rem < 56 {
+        padded.extend_from_slice(&pad[..56 - rem]);
+    } else {
+        // gotta fill up this block then do 56 bytes in the next one
+        padded.extend_from_slice(&pad[..56] as &[u8]);
+        for _ in 0..64 - rem {
+            padded.push(0u8);
+        }
+    }
+
+    // last 8 bytes is the length of the msg in bits
+    let bitlen = len * 8;
+    let bitlen_as_block = [
+        ((bitlen >> 56) & 0xff) as u8,
+        ((bitlen >> 48) & 0xff) as u8,
+        ((bitlen >> 40) & 0xff) as u8,
+        ((bitlen >> 32) & 0xff) as u8,
+        ((bitlen >> 24) & 0xff) as u8,
+        ((bitlen >> 16) & 0xff) as u8,
+        ((bitlen >>  8) & 0xff) as u8,
+        ((bitlen >>  0) & 0xff) as u8,
+    ];
+    padded.extend_from_slice(&bitlen_as_block[..] as &[u8]);
+    padded
+}
--- rust/cryptopals/src/mac_test.rs
+++ rust/cryptopals/src/mac_test.rs
@@ -1,7 +1,9 @@
-use mac::sha1_cat_mac;
+use mac::{sha1_cat_mac, sha1_pad};
 
 pub fn mac_test() {
     println!("sha1_cat_mac {:?}", &sha1_cat_mac("abcd".as_bytes(),
                                                 "efgh".as_bytes()));
+    let padded = sha1_pad("beep boop".as_bytes());
+    println!("sha1_pad {:?} {}", padded, padded.len());
     println!("Finished MAC tests");
 }
